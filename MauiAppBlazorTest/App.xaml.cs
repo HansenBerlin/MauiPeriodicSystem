aa3cd7d33c89a3bc148c0c278e000afff28355d4
Hannes Roever <h.roever@zertificon.com>
2022-12-25 14:25:14 +0100

    -

---
+++ MauiAppBlazorTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppBlazorTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiAppBlazorTest/Interfaces/IThemeEnvironment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppBlazorTest.Interfaces
+{
+    public interface IThemeEnvironment
+    {
+#if __ANDROID__
+    void SetStatusBarColor(Android.Graphics.Color color, bool darkStatusBar);
+#endif
+    }
+}
+
+
